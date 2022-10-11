1bccc9b7bf41638f5dbcaffe2bae84eb11c897f0
Nicolas Buscemi <nicolas.buscemi@student.ehb.be>
2022-10-11 11:24:33 +0200

    Spaceinvader END

--- spaceInvader.js
+++ spaceInvader.js
@@ -1,3 +1,23 @@
 "use strict";
 
-alert("Draw your space invader here");
+drawRectangle();
+
+function drawRectangle() {
+    let canvas = document.querySelector("canvas");
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    let context = canvas.getContext("2d");
+
+
+    context.fillRect(0, 0, 300, 300);
+
+
+
+    context.fillStyle = "#66FF00";
+    context.fillRect(25, 25, 100, 50);
+    context.fillRect(175, 25, 100, 50);
+    context.fillRect(25, 225, 250, 50);
+    context.fillRect(25, 125, 250, 50);
+
+    context.stroke();
+}
